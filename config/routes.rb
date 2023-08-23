6aa1d011eea6f504f9d212a0800ab03e14854269
Andres Castellani <andresadc@gmail.com>
2023-08-23 16:47:52 +0200

    Yeld MVP on rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources :restaurants, only: [:index, :new, :create, :show]
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Creating restaurant"
+
+Restaurant.create(name: 'Le trois cochons', address: '123 Paris', phone_number: '0232345045', category: 'french')
+
+Restaurant.create(name: 'Mamma mia', address: '123 Milan', phone_number: '0432345045', category: 'italian')
+
+Restaurant.create(name: 'Sun and sky', address: '123 Geneva', phone_number: '0632345045', category: 'chinese')
+
+Restaurant.create(name: 'Le trois cochons', address: '123 Torino', phone_number: '0932345045', category: 'italian')
+
+Restaurant.create(name: 'Frites', address: '123 Brujas', phone_number: '0132345045', category: 'belgian')
+
+puts "5 restaurants had been created"
+
+# Review.new(content: "blablabla", rating: 5, restaurant_id: first.id)
